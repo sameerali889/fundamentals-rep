67f479f9de7fadb8f29bbef157dae7e41b9ebd69
sameerali889 <128306877+sameerali889@users.noreply.github.com>
2026-07-30 01:03:22 +0530

    completed num types

---
+++ new-sessions/variable.py
@@ -0,0 +1,29 @@
+"""num1=10
+print(num1)
+fruiT="banana"
+print(fruiT)"""
+
+"""x= 10
+y= 5    
+z =x + y
+print(x)
+print(y)
+print(z)"""
+
+"""x=1.0
+y=2.0
+z="ali"
+k="True"
+print(x)
+print(y)
+print(z)
+print(k)
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(k))"""""
+
+x =-0.9
+print(x)
+print(type(x))
+
