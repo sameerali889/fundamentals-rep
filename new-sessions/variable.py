68de7b37d8222b15bf1de4e0d95b8f18674a157d
sameerali889 <128306877+sameerali889@users.noreply.github.com>
2026-08-02 15:47:50 +0530

    first commit

--- new-sessions/variable.py
+++ new-sessions/variable.py
@@ -38,8 +38,57 @@ y = 4
 print(x + y == 9)
 """""
 
-str1= "Apple"
-print(str1)
-print(type(str1))
-print(len(str1)!=5)
+#str1= "Apple"
+#print(str1)
+#print(type(str1))
+#print(len(str1)!=5)
+#string manipulation functions
 
+# upper() changes from lower case to upper case ex:
+#tr1= "Apple"
+#print(str1.upper())
+
+# lower() changes from upper case to lower case ex:
+#str1= "Kiwi"
+#print(str1.lower())
+#capitalize()
+#str1 = "my dear friend"
+#print(str1.capitalize())
+"""str3 = "Danny is a good boy"
+print(str3.capitalize())"""
+# title() :first letter of each word will be capitalized
+"""str1 = "my dear friend"
+print(str1.title())
+str2 = "Ashraf Ali My Boy"
+print(str2.title())"""
+# swapcase():changes upper case to lower case and lower case to upper case
+"""str1 = "My dear friend"
+print(str1.swapcase())
+str2 = "Ashraf Ali My Boy"
+print(str2.swapcase())"""""
+
+#index() : returns the index of the first occurrence of the specified value
+#str1 ="ABCDEFGHIJKLMN"
+#print(str1.index("C"))
+#print(str1.index("A"))
+#print(str1.index("D"))
+
+#rindex() : returns the index of the last occurrence of the specified value from the right side
+str1 ="ABCDEFGHIJKLMN"
+print(str1.rindex("C"))
+#.find() : returns the index of the first occurrence of the specified value
+"""str1 ="ABCDEF"
+print(str1.find("C"))
+print(str1.find("A"))
+print(str1.find("D"))  
+print(str1.find("Z"))"""  # returns -1 if the value is not found
+#.rfind() : returns the index of the last occurrence of the specified value from the right side
+#str1 ="ABCDEF"
+#print(str1.rfind("C"))
+
+"""x=10
+y=x
+x=20
+print(y)
+a=b=c=5
+print(a,b,c)"""""
