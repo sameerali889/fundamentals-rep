f639308a4c0090b24d23c72da3de23458d7bd5cc
sameerali889 <128306877+sameerali889@users.noreply.github.com>
2026-08-06 17:15:20 +0530

    tuple done

---
+++ new-sessions/elif.py
@@ -0,0 +1,11 @@
+# Conditional statements :
+# if 
+# elif
+# else
+
+#  write a prgm to check if user entered number is divisble by 3 and 5 or not if yes print divisble if not print not divisble
+number =int(input("enter number"))
+if(number % 3 ==0 and number % 5 ==0):
+    print("div")
+else:
+    print("no")
