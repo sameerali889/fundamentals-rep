0804bc3cc87c66782f067edfb194b5c0d28e61d1
sameerali889 <128306877+sameerali889@users.noreply.github.com>
2026-08-09 01:06:12 +0530

    dict done

--- new-sessions/Dict.py
+++ new-sessions/Dict.py
@@ -3,10 +3,11 @@
 # indexing using Variable
 # Variables cannot be duplicated
 # in dictionary those variables are reffered as keys
- #value can be of any data type  int,float,string
-# (int,float,stringd = {"a": 1.23, "b": 4.56}
+ #value can be of any data type  int,float,string,float
+# d = {"a": 1.23, "b": 4.56}
 # print(d["a"])  # 1.23)
-
+# in dictionary the variable can contain key - value pair where key is followed by value of any type
+# Values can be any type, including float, int, str, lists, objects, etc.
 # Students =["Sameer","giya","varma","Niha"]
 
 # students = ["Sameer", "Nida", "Daha", "Altaf"]
@@ -18,3 +19,49 @@
 # students_height = {"alan":154, "sara":120, 'david':144, 'christopher':160}
 
 # students_genders = {"David": True, "Sara": False, "Kiran": False, "Ajay":True, "Shahista":False}
+
+#emp_info ={"Sameer":[1,'America','kansas','ms']} 
+
+# emp_info ={
+#     101 ={"first name":'sameer','position':'hr',"salary":'34000'}
+#     102 ={"first name":'taha','position':'md',"salary":'54000'}
+    #       key      :value
+# squares ={1:1,2:4,3:9,4:16,5:25}
+# print(squares)
+# print(type(squares))
+# 1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+# <class 'dict'>
+# #Indexing:
+# # students ={1:"adam",2:"steph",3:"shiva",4:"vamshi",5:"sundari"}
+# # students_gender ={"adam":True,"steph":False ,"vamshi":True,"sundari":False}
+# # # # print(students)
+# # # print(type(students))
+# # # print(students[1])
+# # # print(students[3])
+# # print(students_gender["vamshi"])
+# #  in-bulit functions of doct
+# #dict1={"A":65,"B":66,"C":67,"d":90}
+# # print(list(dict1.keys())) # dict_keys(['A', 'B', 'C', 'd'])
+# # print(tuple(dict1.values())) # dict_values([65, 66, 67, 90])
+# # print(set(dict1.items())) # dict_items([('A', 65), ('B', 66), ('C', 67), ('d', 90)])
+# # ['A', 'B', 'C', 'd'] (65, 66, 67, 90)
+# # {('C', 67), ('A', 65), ('B', 66), ('d', 90)}
+# # print(dict1.get("C"))
+# # print(type(dict1))
+# # print(dict1["C"])
+# # # In bulit functions of dict:
+# dict1 ={"A":65,"B":66,"C":67,"d":90,"z":100}
+# # print(dict1.keys())
+# # print(dict1.values())
+# # print(dict1.items())
+# dict1.popitem()
+# dict1.clear()
+# print(dict1)]
+# students =["sameer","taha","nida","altaf"]
+# stud_results =dict.fromkeys(students,"fail")
+# print(stud_results)
+# dict3 =dict([("ajay","male"),("sara","female"),("david","female"),("afroz","male")])
+# print(dict3)
+# print(type(dict3))
+
+# Dict Completed
